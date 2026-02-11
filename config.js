36a8ad7e1d02ad7d1060a409e5a3c33f847b5ada
Alexander Mackey <alexmackey1@gmail.com>
2026-02-11 16:12:06 -0500

    Update GitHub owner and repository in config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.CIH_CONFIG = {
-  githubOwner: "YOUR_GITHUB_ORG",
-  githubRepo: "YOUR_REPO_NAME",
+  githubOwner: "fs-cih",
+  githubRepo: "cihgrants",
   githubBranch: "main",
   // Fine-grained PAT with Actions: Read and write + Contents: Read and write.
   // Required for browser-triggered workflow_dispatch API calls.
